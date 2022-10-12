ffef3c31013a4e36e783bb8f98744b9eca3d8842
Gabriela Muniz <gabrielanamu@live.com>
2022-10-11 21:02:55 -0300

    adiciona requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,13 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('verifica se localStorage.setItem é chamado', () => {
+    saveCartItems('ID: MLB2663207668 | TITLE: Microsoft Office 365 Personal 1 Usuário (box) Qq2-01386 | PRICE: $99');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  })
+
+  it('verifica se localStorage.setItem foi chamado com parâmetros especificos', () => {
+    saveCartItems('ID: MLB2663207668 | TITLE: Microsoft Office 365 Personal 1 Usuário (box) Qq2-01386 | PRICE: $99');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItem', 'ID: MLB2663207668 | TITLE: Microsoft Office 365 Personal 1 Usuário (box) Qq2-01386 | PRICE: $99');
+  })
 });
